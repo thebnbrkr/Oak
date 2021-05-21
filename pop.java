1f8f715b4117b0341a011557ea49c3e73b7f5acc
thebnbrkr <51245304+thebnbrkr@users.noreply.github.com>
2021-05-21 05:45:10 +0530

    Add files via upload

---
+++ pop.java
@@ -0,0 +1,11 @@
+
+public class pop {
+	public static void main (String[]args){
+	double x = 686876.098980;
+	double radius = x;
+	double area = radius*radius*3.14;	
+	System.out.println(area); //should print the area
+	System.out.println(x); //should print x
+	System.out.println("The area is "+area+", and the radius is "+x+"."); 
+	}
+}
